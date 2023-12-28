a006427776452b8e86313c6d337a441e79d24d88
zengchen1024 <chenzeng2@huawei.com>
2023-12-28 23:02:45 +0800

    init git server

---
+++ main.go
@@ -0,0 +1,54 @@
+// Copyright 2014 The Gogs Authors. All rights reserved.
+// Copyright 2016 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"os"
+	"runtime"
+	"strings"
+	"time"
+
+	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/setting"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/openmerlin/gitea_data/cmd"
+)
+
+// these flags will be set by the build flags
+var (
+	Version     = "development" // program version for this build
+	Tags        = ""            // the Golang build tags
+	MakeVersion = ""            // "make" program version if built with make
+)
+
+func init() {
+	setting.AppVer = Version
+	setting.AppBuiltWith = formatBuiltWith()
+	setting.AppStartTime = time.Now().UTC()
+}
+
+func main() {
+	cli.OsExiter = func(code int) {
+		log.GetManager().Close()
+		os.Exit(code)
+	}
+	app := cmd.NewMainApp(Version, formatBuiltWith())
+	_ = cmd.RunMainApp(app, os.Args...) // all errors should have been handled by the RunMainApp
+	log.GetManager().Close()
+}
+
+func formatBuiltWith() string {
+	version := runtime.Version()
+	if len(MakeVersion) > 0 {
+		version = MakeVersion + ", " + runtime.Version()
+	}
+	if len(Tags) == 0 {
+		return " built with " + version
+	}
+
+	return " built with " + version + " : " + strings.ReplaceAll(Tags, " ", ", ")
+}
